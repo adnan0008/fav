581a855322c0c80907d184a221462c79de11a29e
adnan0008 <adnannazir.comsats@gmail.com>
2022-03-29 14:02:01 -0700

    end of the story

--- src/app/main/sample/home.component.ts
+++ src/app/main/sample/home.component.ts
@@ -1,3 +1,4 @@
+
 import { HttpService } from "./../../service/http.service";
 import { Component, OnInit } from "@angular/core";
 import Chart from "chart.js";
@@ -8,49 +9,52 @@ import Chart from "chart.js";
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-charts: any = [];
-//  labels = Utils.months({count: 7});
-// labels = ''
-//  data = {
-//   labels: this.labels,
-//   datasets: [{
-//     label: 'My First Dataset',
-//     data: [65, 59, 80, 81, 56, 55, 40],
-//     fill: false,
-//     borderColor: 'rgb(75, 192, 192)',
-//     tension: 0.1
-//   }]
-// };
-// config = {
-//   type: 'line',
-//   data: this.data,
-// };
-//   // Do not show lines for all datasets by default
-// // Chart.defaults.datasets.line.showLine = false;
+  charts: any = [];
+  neutralArr: any[] = [];
+  positiveArr: any[] = [];
+  negativeArr: any[] = [];
+  //  labels = Utils.months({count: 7});
+  // labels = ''
+  //  data = {
+  //   labels: this.labels,
+  //   datasets: [{
+  //     label: 'My First Dataset',
+  //     data: [65, 59, 80, 81, 56, 55, 40],
+  //     fill: false,
+  //     borderColor: 'rgb(75, 192, 192)',
+  //     tension: 0.1
+  //   }]
+  // };
+  // config = {
+  //   type: 'line',
+  //   data: this.data,
+  // };
+  //   // Do not show lines for all datasets by default
+  // // Chart.defaults.datasets.line.showLine = false;
 
-// // This chart would show a line only for the third dataset
- 
-//  chart = new Chart(new CanvasRenderingContext2D(), {
-//     type: 'line',
-//     data: {
-//         datasets: [{
-//             data: [0, 0],
-//         }, {
-//             data: [0, 1]
-//         }, {
-//             data: [1, 0],
-//             showLine: true // overrides the `line` dataset default
-//         }, {
-//             type: 'scatter', // 'line' dataset default does not affect this dataset since it's a 'scatter'
-//             data: [1, 1]
-//         }]
-//     }
-// });
+  // // This chart would show a line only for the third dataset
+
+  //  chart = new Chart(new CanvasRenderingContext2D(), {
+  //     type: 'line',
+  //     data: {
+  //         datasets: [{
+  //             data: [0, 0],
+  //         }, {
+  //             data: [0, 1]
+  //         }, {
+  //             data: [1, 0],
+  //             showLine: true // overrides the `line` dataset default
+  //         }, {
+  //             type: 'scatter', // 'line' dataset default does not affect this dataset since it's a 'scatter'
+  //             data: [1, 1]
+  //         }]
+  //     }
+  // });
 
   products: any[] = [];
-  productReviews:any[] = [];
+  productReviews: any[] = [];
   public openSearchRef = false;
-  public searchText = '';
+  public searchText = "";
   constructor(private httpService: HttpService) {}
 
   public contentHeader: object;
@@ -72,7 +76,7 @@ charts: any = [];
             name: "Home",
             isLink: true,
             link: "/",
-          }
+          },
           // {
           //   name: "Sample",
           //   isLink: false,
@@ -80,92 +84,63 @@ charts: any = [];
         ],
       },
     };
- 
   }
 
   searchUpdate(event) {
-    if(event.target.value.length){
+    if (event.target.value.length > 1) {
+      this.neutralArr = [];
+      this.positiveArr = [];
+      this.negativeArr = [];
       const val = event.target.value.toLowerCase();
       const formData = new FormData();
       formData.append("search", val);
       // , {headers:{'Authorization': `Token ${localStorage.getItem('token')}`}}
       this.httpService.post("api/get", formData).subscribe((response) => {
-        if(response && response?.get_json ){
+        console.log(response);
+        for (let data of response?.get_json) {
+          for (let value of data?.review_data) {
+            if (value?.status?.toLowerCase() == "neutral") {
+              this.neutralArr.push(value?.status);
+            } else if (value?.status?.toLowerCase() == "positive") {
+              this.positiveArr.push(value?.status);
+            } else if (value?.status?.toLowerCase() == "negative") {
+              this.negativeArr.push(value?.status);
+            }
+          }
+        }
+
+        if (response && response?.get_json) {
           this.products = response?.get_json;
-          const ctx = document.querySelector('canvas').getContext('2d');
-          this.charts = new Chart( ctx, {
-type:'line',
-data: {
-  datasets: [{
-    label:'Neutral',
-      data: [0, 1],
-      backgroundColor: 'rgba(0, 99, 132, 0.2)',
-  }, {
-    label:'Negative',
-      data: [5, 1],
-      backgroundColor: 'rgb(255, 99, 132)'
-  }, {
-    label:'Positive',
-      data: [3, 0],
-      // showLine: true, // overrides the `line` dataset default
-      backgroundColor: 'rgb(255, 99, 0)'
-  },
-  //  {
-  //     type: 'scatter', // 'line' dataset default does not affect this dataset since it's a 'scatter'
-  //     data: [1, 1]
-  // }
-]
-}
-          })}})}
-        
-
-
-          // const divCol = document.createComment('div');
-          // let canvas = document.createElement('canvas');
-          // canvas = divCol?.appendChild(canvas);
-          // canvas.setAttribute('id', 'chart');
-          // document.getElementById('charts')?.appendChild(divCol);
-          // document.getElementById('charts');
-          // const id = <HTMLCanvasElement> document.getElementById('charts');
-        //  const ctx = <HTMLElement>document.getElementById('charts').getContext('2d');
-//         const canvas = <HTMLCanvasElement> document.getElementById('charts');
-// const ctx = canvas.getContext('2d');
-//           this.charts = new Chart( ctx, {
-// type:'line',
-// data: {
-//   datasets: [{
-//     label:'first',
-//       data: [0, 1],
-//       backgroundColor: [
-//         'rgba(255, 99, 132, 0.2)',
-//         'rgba(54, 162, 235, 0.2)',
-//         'rgba(255, 206, 86, 0.2)',
-//     ],
-//   }, {
-//     label:'second',
-//       data: [0, 1]
-//   }, {
-//     label:'third',
-//       data: [1, 0],
-//       showLine: true // overrides the `line` dataset default
-//   },
-//   //  {
-//   //     type: 'scatter', // 'line' dataset default does not affect this dataset since it's a 'scatter'
-//   //     data: [1, 1]
-//   // }
-// ]
-// }
-//           })
-//         }
-      
-//       });
-//     }
-
-    // if (val !== '') {
-    //   this.document.querySelector('.app-content').classList.add('show-overlay');
-    // } else {
-    //   this.document.querySelector('.app-content').classList.remove('show-overlay');
-    // }
-    // this.autoSuggestion(event);
+          const ctx = document.querySelector("canvas").getContext("2d");
+          
+          this.charts = new Chart(ctx, {
+            type: "line",
+         
+            data: {
+              datasets: [
+                {
+                  label: "Neutral",
+                  data: [0, this.neutralArr.length],
+                  backgroundColor: "#C0C0C0",
+                  
+                  
+                },
+                {
+                  label: "Negative",
+                  data: [0, this.negativeArr.length],
+                  backgroundColor: "#FF0000",
+                },
+                {
+                  label: "Positive",
+                  data: [0, this.positiveArr.length],
+                  backgroundColor: "#008000",
+                },
+              
+              ],
+            },
+          });
+        }
+      });
+    }
   }
 }
